6b3dd1ee1bc4d449261d252aca1ccfb2a90bae2d
IamShaDoW666 <www.gaming2018@gmail.com>
2024-06-24 15:25:01 +0530

    init

---
+++ src/data/socials.ts
@@ -0,0 +1,23 @@
+export const socials = [
+  {
+    title: 'Instagram',
+    icon: 'mdi:instagram',
+    url: 'https://www.instagram.com',
+  },
+  {
+    title: 'Twitter',
+    icon: 'mdi:twitter',
+    url: 'https://twitter.com',
+  },
+  {
+    title: 'YouTube',
+    icon: 'mdi:youtube',
+    url: 'https://youtube.com',
+  },
+  {
+    title: 'Facebook',
+    icon: 'mdi:facebook',
+    url: 'https://facebook.com',
+  },
+  
+]
